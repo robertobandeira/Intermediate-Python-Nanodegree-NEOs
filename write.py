b49ab93d222b0662400a8697a8dea8ea4350a825
Roberto Bandeira de Mello <roberto.bandeiram@gmail.com>
2023-06-16 14:18:45 -0400

    Finished all tasks and passes all unit tests

--- write.py
+++ write.py
@@ -12,6 +12,7 @@ You'll edit this file in Part 4.
 """
 import csv
 import json
+import math
 
 
 def write_to_csv(results, filename):
@@ -28,12 +29,11 @@ def write_to_csv(results, filename):
         'datetime_utc', 'distance_au', 'velocity_km_s',
         'designation', 'name', 'diameter_km', 'potentially_hazardous'
     )
-    # TODO: Write the results to a CSV file, following the specification in the instructions.
     with open(filename, 'w') as outfile:
         writer = csv.writer(outfile)
         writer.writerow(fieldnames)
         for r in results:
-            row = [r.time, r.distance, r.velocity, r._designation, r.neo.name,r.neo.diameter,str(r.neo.hazardous)]
+            row = [r.time_str, r.distance, r.velocity, r._designation, r.neo.name,r.neo.diameter,str(r.neo.hazardous)]
             writer.writerow(row)
 
 
@@ -48,4 +48,20 @@ def write_to_json(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO: Write the results to a JSON file, following the specification in the instructions.
+    transformed_results = []
+    for r in results:
+        temp = dict(
+            datetime_utc=r.time_str,
+            distance_au=r.distance,
+            velocity_km_s=r.velocity,
+            neo=dict(
+                designation=r.neo.designation,
+                name=r.neo.name if r.neo.name is not None else '',
+                diameter_km=r.neo.diameter,
+                potentially_hazardous=r.neo.hazardous
+            )
+        )
+        transformed_results.append(temp)
+        print(transformed_results)
+    with open(filename, 'w') as outfile:
+        json.dump(transformed_results, outfile)
